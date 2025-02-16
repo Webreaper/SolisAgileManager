98af8739f22788b416ab0f399ce79a50b5139f01
Mark Otway <mark@otway.com>
2025-02-16 10:13:17 +0000

    Add Jitter to Solis API #111

--- SolisManager/APIWrappers/SolisAPI.cs
+++ SolisManager/APIWrappers/SolisAPI.cs
@@ -403,10 +403,20 @@ public class SolisAPI
         return default;
     }
 
+    private static Random random = new Random();
+    
+    private async Task Jitter()
+    {
+        var timeMs = random.Next(50, 3000);
+        await Task.Delay(timeMs);
+    }
+    
     private async Task<string> Post(string url, string content)
     {
         try
         {
+            await Jitter();
+            
             var request = new HttpRequestMessage(HttpMethod.Post, url);
             var date = DateTime.UtcNow.ToString("ddd, d MMM yyyy HH:mm:ss 'GMT'");
 
