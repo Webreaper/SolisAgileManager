c51ab5eb48199a72a8640b9cd33430b4fec379f1
Mark Otway <mark@otway.com>
2026-08-28 12:59:46 +0100

    Workig

--- SolisManager.Shared/Models/PricePlanSlot.cs
+++ SolisManager.Shared/Models/PricePlanSlot.cs
@@ -28,6 +28,8 @@ public record OctopusRate
     public decimal value_inc_vat { get; set; }
     public DateTime valid_from { get; set; }
     public DateTime? valid_to { get; set; } = DateTime.MaxValue;
+
+    public override string ToString() => $"{valid_from:dd-MMM-yyyy HH:mm} - {valid_from:dd-MMM-yyyy HH:mm}: {value_inc_vat:N3}p/kWh";
 }
 
 public record PricePlanSlot
